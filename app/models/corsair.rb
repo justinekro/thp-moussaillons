42d5d2d64b69ed78791c62f0eb961f9c56df9bdb
Justine <>
2018-02-01 00:37:46 +0100

    ok

--- app/models/corsair.rb
+++ app/models/corsair.rb
@@ -1,6 +1,2 @@
 class Corsair < ApplicationRecord
-	  validates :first_name, presence: true
-	  validates	:age, presence: true
-	  validates_numericality_of :age, :greater_than => 15
-	  validates :likeness, presence: true
 end
